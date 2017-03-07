b9603105e461381436b34d0d9ba899cbf41355f6
Toan Nguyen <khanhtoan.nguyen@protonmail.ch>
2017-03-07 13:59:45 +0200

    Add project files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour {
+
+	/*private float moveSpeed = 40.0f;
+
+	public Transform target;
+	Camera camera;
+
+	// Update is called once per frame
+	// FixedUpdate should be used instead of Update when dealing with Rigidbody.
+	void FixedUpdate () {
+		float moveX = Input.GetAxis("Horizontal");
+		float moveZ = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(moveX, 0f, moveZ);
+		GetComponent<Rigidbody> ().velocity = movement * moveSpeed;
+	} */
+}
